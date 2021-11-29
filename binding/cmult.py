a8676614f1fefc40ad2b9defe779c10c3ef00477
charles.lee <cheoljoo.lee@lge.com>
2021-11-29 17:46:00 +0900

    cmult

--- binding/cmult.py
+++ binding/cmult.py
@@ -22,10 +22,10 @@ if __name__ == "__main__":
 
     # This produces a bad answer:
     answer = c_lib.cmult(x, ctypes.c_float(y))
-    print(f"    In Python: int: {x} float {y:.1f} return val {answer:.1f}")
+    print(f"    In Python: int: {x} * float {y:.1f} return val {answer:.1f}")
     print()
 
     # You need tell ctypes that the function returns a float
     c_lib.cmult.restype = ctypes.c_float
-    answer = c_lib.cmult(x, ctypes.c_float(y))
-    print(f"    In Python: int: {x} float {y:.1f} return val {answer:.1f}")
+    #answer = c_lib.cmult(x, ctypes.c_float(y))
+    #print(f"    In Python: int: {x} float {y:.1f} return val {answer:.1f}")
